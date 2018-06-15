a26d24eec80dca692c531af5a65110c3061c68c7
TPCreative <tpcreative.co@gmail.com>
2018-06-15 22:02:30 +0700

    Initial Commit

---
+++ IOSWithRealmDatabase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSWithRealmDatabase
+//
+//  Created by Mac10 on 6/15/18.
+//  Copyright © 2018 Mac10. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
