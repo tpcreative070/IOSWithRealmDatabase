bf02228de7f3fc38b17d024be2678cd658445be9
TPCreative <tpcreative.co@gmail.com>
2018-06-17 15:21:07 +0700

    first commit

---
+++ IOSWithRealmDatabase/model/TaskManager.swift
@@ -0,0 +1,13 @@
+//
+//  TaskManager.swift
+//  IOSWithRealmDatabase
+//
+//  Created by Mac10 on 6/16/18.
+//  Copyright © 2018 Mac10. All rights reserved.
+//
+
+import UIKit
+
+class TaskManager: NSObject {
+
+}
