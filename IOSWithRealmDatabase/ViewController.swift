5c97c4a8488b76ad9b7cdc621d856f015091f2a6
TPCreative <tpcreative.co@gmail.com>
2018-06-17 17:36:59 +0700

    Final commit

--- IOSWithRealmDatabase/ViewController.swift
+++ IOSWithRealmDatabase/ViewController.swift
@@ -8,18 +8,139 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController ,UITextFieldDelegate{
+    
+    @IBOutlet weak var textFieldInput: UITextField!
+    @IBOutlet weak var tbView: UITableView!
+    var mList = [TaskManager]()
+    var task : TaskManager!
+    var isEdit : Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.textFieldInput.delegate = self
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        updateView()
+    }
+    
+    func updateView(){
+        TaskManager.shared.getAll{ [weak self] list in
+            self?.mList = list
+            self?.tbView.reloadData()
+            self?.textFieldInput.text = ""
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @IBAction func btnSend(_ sender: UIButton) {
+        if !(textFieldInput.text?.isEmpty)! {
+            if isEdit{
+                if let mTask  = task {
+                    let task = TaskManager()
+                    task.key = mTask.key
+                    task.task = textFieldInput.text!
+                    task.date = TaskManager.shared.getCurrentDateTime()
+                    TaskManager.shared.created(task: task)
+                }
+            }else{
+                let task = TaskManager()
+                task.task = textFieldInput.text!
+                task.key = String(TaskManager.shared.currentTimeInMiliseconds())
+                task.date = TaskManager.shared.getCurrentDateTime()
+                TaskManager.shared.created(task: task)
+            }
+            updateView()
+        }
+        isEdit = false
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        textFieldInput.resignFirstResponder()
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        // let duration = notification.userInfo?[UIKeyboardAnimationDurationUserInfoKey]
+        // print("duration",duration)
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            let keyboardHeight : Int = Int(keyboardSize.height)
+            print("keyboardWillShow",keyboardHeight)
+            if let height = UserDefaults.standard.value(forKey: "keyboardHeight") as? (Int) {
+                moveTextField(textFieldInput, moveDistance: -height as Int, moveDuration: 0.43, up: true)
+            }else{
+                UserDefaults.standard.set(keyboardHeight, forKey: "keyboardHeight")
+                moveTextField(textFieldInput, moveDistance: -keyboardHeight, moveDuration: 0.43, up: true)
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification){
+        if let height = UserDefaults.standard.value(forKey: "keyboardHeight") as? (Int) {
+            moveTextField(textFieldInput, moveDistance: -height as Int, moveDuration: 0.25, up: false)
+        }
+    }
 
-
+    func moveTextField(_ textField: UITextField, moveDistance: Int, moveDuration: Double, up: Bool) {
+        let movement: CGFloat = CGFloat(up ? moveDistance : -moveDistance)
+        UIView.beginAnimations("animateTextField", context: nil)
+        UIView.setAnimationBeginsFromCurrentState(true)
+        UIView.setAnimationDuration(moveDuration)
+        self.view.frame = self.view.frame.offsetBy(dx: 0, dy: movement)
+        UIView.commitAnimations()
+    }
 }
+
+extension ViewController : UITableViewDelegate {
+    
+}
+
+extension ViewController : UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "identifier")
+        let staff = mList[indexPath.row]
+        cell.textLabel?.text = staff.task
+        cell.detailTextLabel?.text = "\(staff.date)"
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let moreRowAction = UITableViewRowAction(style: UITableViewRowActionStyle.default, title: "Edit", handler:{action, indexpath in
+            self.task = self.mList[indexPath.row]
+            self.isEdit = true
+            self.textFieldInput.text = self.task.task
+            print("MORE•ACTION")
+        });
+        moreRowAction.backgroundColor = UIColor(red: 0.298, green: 0.851, blue: 0.3922, alpha: 1.0);
+        let deleteRowAction = UITableViewRowAction(style: UITableViewRowActionStyle.default, title: "Delete", handler:{action, indexpath in
+            self.isEdit = false
+            print("DELETE•ACTION")
+            TaskManager.shared.delete(task : self.mList[indexPath.row])
+            self.updateView()
+        });
+        return [deleteRowAction, moreRowAction]
+    }
+    
+}
+
+
+
 
--- IOSWithRealmDatabase/model/TaskManager.swift
+++ IOSWithRealmDatabase/model/TaskManager.swift
@@ -7,7 +7,91 @@
 //
 
 import UIKit
+import RealmSwift
 
-class TaskManager: NSObject {
+class TaskManager: Object {
 
+    @objc dynamic var key = ""
+    @objc dynamic var task = ""
+    @objc dynamic var date = ""
+    
+    override static func primaryKey() -> String? {
+        return "key"
+    }
+    
+    public class var shared: TaskManager {
+        struct Static {
+            static let instance: TaskManager = TaskManager()
+        }
+        return Static.instance
+    }
+    
+    /*In the case : Creating a task with the same primary key as a previously saved task */
+    func added(task : TaskManager){
+        let realm = try! Realm()
+        try! realm.write() {
+            realm.add(task,update:true)
+        }
+    }
+    
+    /*In the case : Creating a task with the unique primary key as a previously saved task */
+    func created(task : TaskManager){
+        let realm = try! Realm()
+        try! realm.write() {
+            realm.create(TaskManager.self, value: ["key": task.key, "task": task.task, "date" : task.date], update: true)
+        }
+    }
+    
+    func deleteAll(){
+        let realm = try! Realm()
+        try! realm.write() {
+            realm.deleteAll()
+        }
+    }
+    
+    func delete(task : TaskManager){
+        let realm = try! Realm()
+        try! realm.write() {
+            realm.delete(task)
+        }
+    }
+    
+    func getAll(callBack: @escaping ([TaskManager]) -> Void) {
+        let realm = try! Realm()
+        try! realm.write() {
+            let result = realm.objects(TaskManager.self).sorted(byKeyPath: "key", ascending: false)
+            let response = result.toArray(ofType: TaskManager.self)
+            callBack(response)
+            print(result)
+        }
+    }
+    
+    func getCurrentDateTime() -> String{
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+        let currentDateTime = Date()
+        let result = formatter.string(from: currentDateTime)
+        return result
+    }
+    
+    func currentTimeInMiliseconds() -> Int {
+        let currentDate = Date()
+        let since1970 = currentDate.timeIntervalSince1970
+        return Int(since1970 * 1000)
+    }
+    
+}
+
+extension Int {
+    //Milliseconds to date
+    func dateFromMilliseconds() -> Date {
+        return Date(timeIntervalSince1970: TimeInterval(self)/1000)
+    }
+}
+
+extension Results {
+    func toArray<T>(ofType: T.Type) -> [T] {
+        let array = Array(self) as! [T]
+        return array
+    }
 }
